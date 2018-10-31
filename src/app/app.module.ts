b79afe96f39cd542bf90d3ba6635aac81367d334
Thomas Aucott <tafolder@gmail.com>
2018-10-31 13:15:09 +0000

    test-build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about'
+import { StartPage } from '../pages/start/start'
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AboutPage,
+    StartPage
+
   ],
   imports: [
     BrowserModule,
@@ -19,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AboutPage,
+    StartPage
+
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
+
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AboutPage } from '../../pages/about/about'
+import { StartPage } from '../../pages/start/start'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
+
+aboutSwitch() {
+  this.navCtrl.push(AboutPage)
+}
+
+ startSwitch() {
+  this.navCtrl.push(StartPage)
+}
 
   }
-
-}
